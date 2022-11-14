f544262d64aed2f8954a66a9ff2bfc1ceb651b35
xjh <xjh19972@gmail.com>
2022-11-14 14:14:05 -0500

    update default

--- train_satellite_satellite_full.sh
+++ train_satellite_satellite_full.sh
@@ -3,4 +3,4 @@
 eval "$(conda shell.bash hook)"
 conda activate VTL
 
-python3 train.py --dataset_name=satellite_0_satellite_1 --mining=full --datasets_folder=datasets --infer_batch_size 128 --train_batch_size 10 --use_faiss_gpu --neg_samples_num 10000
+python3 train.py --dataset_name=satellite_0_satellite_1 --mining=full --datasets_folder=datasets --infer_batch_size 128 --train_batch_size 10 --use_faiss_gpu --neg_samples_num 10000 --lr 0.0001
