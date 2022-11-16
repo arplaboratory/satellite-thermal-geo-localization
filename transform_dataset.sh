cc0a32c34174afc28b2f0d4cbd0516897a527450
xjh <xjh19972@gmail.com>
2022-11-16 17:28:19 -0500

    update script

--- transform_dataset.sh
+++ transform_dataset.sh
@@ -43,22 +43,22 @@ rm -r ./datasets/satellite_0_thermalmapping_4
 rm -r ./datasets/satellite_0_thermalmapping_5
 
 # thermal_1 + thermal_1
-python h5_transformer.py --database_name satellite --database_index 0 --queries_name thermalmapping --queries_index 0 --compress --train_sample_num 10000 --val_sample_num 10000 --region_num 2
+python h5_transformer.py --database_name thermalmapping --database_index 0 --queries_name thermalmapping --queries_index 0 --compress --train_sample_num 10000 --val_sample_num 10000 --region_num 2
 
 # thermal_2 + thermal_2
-python h5_transformer.py --database_name satellite --database_index 0 --queries_name thermalmapping --queries_index 1 --compress --train_sample_num 10000 --val_sample_num 10000 --region_num 2
+python h5_transformer.py --database_name thermalmapping --database_index 1 --queries_name thermalmapping --queries_index 1 --compress --train_sample_num 10000 --val_sample_num 10000 --region_num 2
 
 # thermal_3 + thermal_3
-python h5_transformer.py --database_name satellite --database_index 0 --queries_name thermalmapping --queries_index 2 --compress --train_sample_num 10000 --val_sample_num 10000 --region_num 2
+python h5_transformer.py --database_name thermalmapping --database_index 2 --queries_name thermalmapping --queries_index 2 --compress --train_sample_num 10000 --val_sample_num 10000 --region_num 2
 
 # thermal_4 + thermal_4
-python h5_transformer.py --database_name satellite --database_index 0 --queries_name thermalmapping --queries_index 3 --compress --train_sample_num 10000 --val_sample_num 10000 --region_num 2
+python h5_transformer.py --database_name thermalmapping --database_index 3 --queries_name thermalmapping --queries_index 3 --compress --train_sample_num 10000 --val_sample_num 10000 --region_num 2
 
 # thermal_5 + thermal_5
-python h5_transformer.py --database_name satellite --database_index 0 --queries_name thermalmapping --queries_index 4 --compress --train_sample_num 10000 --val_sample_num 10000 --region_num 2
+python h5_transformer.py --database_name thermalmapping --database_index 4 --queries_name thermalmapping --queries_index 4 --compress --train_sample_num 10000 --val_sample_num 10000 --region_num 2
 
 # thermal_6 + thermal_6
-python h5_transformer.py --database_name satellite --database_index 0 --queries_name thermalmapping --queries_index 5 --compress --train_sample_num 10000 --val_sample_num 10000 --region_num 2
+python h5_transformer.py --database_name thermalmapping --database_index 5 --queries_name thermalmapping --queries_index 5 --compress --train_sample_num 10000 --val_sample_num 10000 --region_num 2
 
 # thermal_123456 + thermal_123456
 python h5_merger.py --database_name thermalmapping --database_indexes 012345 --queries_name thermalmapping --queries_indexes 012345 --compress --region_num 2
