4dc15e98b904fe82c8176782ddbe3e7765470712
xjh <xjh19972@gmail.com>
2022-11-15 19:05:11 -0500

    delete

--- train_foxtech_satellite_full.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-eval "$(conda shell.bash hook)"
-conda activate VPR_SSL
-
-python3 train.py --dataset_name=satellite_0_foxtechmapping_0 --mining=full --datasets_folder=datasets
--- train_satellite_satellite_full.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-eval "$(conda shell.bash hook)"
-conda activate VTL
-
-python3 train.py --dataset_name=satellite_0_satellite_1 --mining=full --datasets_folder=datasets --infer_batch_size 128 --train_batch_size 10 --use_faiss_gpu --neg_samples_num 10000 --lr 0.0001
