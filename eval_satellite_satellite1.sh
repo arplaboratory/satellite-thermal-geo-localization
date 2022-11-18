3a478dce8e606f85976b7f12c8208a29ef3462e0
xjh <xjh19972@gmail.com>
2022-11-18 11:27:35 -0500

    update scripts

---
+++ eval_satellite_satellite1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+eval "$(conda shell.bash hook)"
+conda activate VTL
+
+python3 eval.py --resume='logs/default/'$1'/best_model.pth' --dataset_name=satellite_0_satellite_1 --datasets_folder ./datasets --infer_batch_size 128 --use_faiss_gpu
+
+python3 eval.py --resume='logs/default/'$1'/best_model.pth' --dataset_name=satellite_0_satellite_1 --datasets_folder ./datasets --infer_batch_size 128 --prior_location_threshold=512 --use_faiss_gpu
--- eval_satellite_thermal.sh
+++ eval_satellite_thermal.sh
@@ -5,7 +5,3 @@ conda activate VTL
 python3 eval.py --resume='logs/default/'$1'/best_model.pth' --dataset_name=satellite_0_thermalmapping_012345 --datasets_folder ./datasets --infer_batch_size 128 --use_faiss_gpu
 
 python3 eval.py --resume='logs/default/'$1'/best_model.pth' --dataset_name=satellite_0_thermalmapping_012345 --datasets_folder ./datasets --infer_batch_size 128 --prior_location_threshold=512 --use_faiss_gpu
-
-python3 eval.py --resume='logs/default/'$1'/best_model.pth' --dataset_name=thermalmapping_012345_thermalmapping_012345 --datasets_folder ./datasets --infer_batch_size 128 --use_faiss_gpu
-
-python3 eval.py --resume='logs/default/'$1'/best_model.pth' --dataset_name=thermalmapping_012345_thermalmapping_012345 --datasets_folder ./datasets --infer_batch_size 128 --prior_location_threshold=512 --use_faiss_gpu
