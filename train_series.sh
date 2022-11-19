ce78336ea842ab20fc7dba3717494d54e1ee9886
xjh <xjh19972@gmail.com>
2022-11-19 15:49:18 -0500

    update scripts

--- train_series.sh
+++ train_series.sh
@@ -12,3 +12,13 @@ conda activate VTL
 ./train_bing_thermal_full.sh
 
 ./train_foxtech_thermal_full.sh
+
+./train_bing_esri_partial.sh
+
+./train_bing_google_partial.sh
+
+./train_bing_foxtech_partial.sh
+
+./train_bing_thermal_partial.sh
+
+./train_foxtech_thermal_partial.sh
