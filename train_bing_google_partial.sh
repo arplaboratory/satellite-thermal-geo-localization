39c6daa776df02a15ba28bccfcf2088820f1f038
xjh <xjh19972@gmail.com>
2022-11-18 19:36:55 -0500

    update script

---
+++ train_bing_google_partial.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+eval "$(conda shell.bash hook)"
+conda activate VTL
+
+python3 train.py --dataset_name=satellite_0_satellite_3 --mining=partial --datasets_folder=datasets --infer_batch_size 128 --train_batch_size 10 --use_faiss_gpu --neg_samples_num 10000 --lr 0.0001
