2f558dd59c9482375ceb37fa3c06e024d902c19f
xjh <xjh19972@gmail.com>
2022-11-26 00:36:42 -0500

    revise

--- train_bing_esri_partial.sh
+++ train_bing_esri_partial.sh
@@ -3,5 +3,4 @@
 eval "$(conda shell.bash hook)"
 conda activate VTL
 
-echo "yea"
 python3 train.py --dataset_name=satellite_0_satellite_1 --mining=partial --datasets_folder=datasets --infer_batch_size 256 --train_batch_size 12 --lr 0.0001
