9fa0a9dc9dcf0b7797adaefd25345fb1a4f7ed2d
xjh <xjh19972@gmail.com>
2022-11-26 00:18:18 -0500

    ban faiss gpu

--- train_bing_google_full.sh
+++ train_bing_google_full.sh
@@ -3,4 +3,4 @@
 eval "$(conda shell.bash hook)"
 conda activate VTL
 
-python3 train.py --dataset_name=satellite_0_satellite_3 --mining=full --datasets_folder=datasets --infer_batch_size 128 --train_batch_size 10 --use_faiss_gpu --lr 0.0001
+python3 train.py --dataset_name=satellite_0_satellite_3 --mining=full --datasets_folder=datasets --infer_batch_size 128 --train_batch_size 10 --lr 0.0001
--- train_series.sh
+++ train_series.sh
@@ -3,22 +3,20 @@
 eval "$(conda shell.bash hook)"
 conda activate VTL
 
-./train_bing_esri_full.sh
+# ./train_bing_esri_full.sh
 
-./train_bing_google_full.sh
+# ./train_bing_google_full.sh
 
-./train_bing_foxtech_full.sh
+# ./train_bing_foxtech_full.sh
 
-./train_bing_thermal_full.sh
+# ./train_bing_thermal_full.sh
 
-./train_foxtech_thermal_full.sh
-
+# ./train_foxtech_thermal_full.sh 
+ 
 ./train_bing_esri_partial.sh
 
-./train_bing_google_partial.sh
+./train_bing_google_partial.sh 
 
 ./train_bing_foxtech_partial.sh
 
-./train_bing_thermal_partial.sh
-
-./train_foxtech_thermal_partial.sh
+./train_bing_thermal_partial.sh 
