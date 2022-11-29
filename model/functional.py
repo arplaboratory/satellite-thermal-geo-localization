7080e591bb2262b214c248e746e53081853ff241
xjh <xjh19972@gmail.com>
2022-11-29 16:41:38 -0500

    remove unnecessary argument

--- model/functional.py
+++ model/functional.py
@@ -26,40 +26,6 @@ def sare_joint(query, positive, negatives):
     # no Exceptions, there would actually be a conceptual error.
     return sare_ind(query, positive, negatives)
 
-def info_nce_loss(features, args):
-    ''' Info nce loss for simclr'''
-    labels = torch.cat([torch.arange(args.batch_size) for i in range(args.n_views)], dim=0)
-    labels = (labels.unsqueeze(0) == labels.unsqueeze(1)).float()
-    labels = labels.to(args.device)
-
-    features = F.normalize(features, dim=1)
-
-    similarity_matrix = torch.matmul(features, features.T)
-    # assert similarity_matrix.shape == (
-    #     self.args.n_views * self.args.batch_size, self.args.n_views * self.args.batch_size)
-    # assert similarity_matrix.shape == labels.shape
-
-    # discard the main diagonal from both: labels and similarities matrix
-    mask = torch.eye(labels.shape[0], dtype=torch.bool).to(args.device)
-    labels = labels[~mask].view(labels.shape[0], -1)
-    similarity_matrix = similarity_matrix[~mask].view(similarity_matrix.shape[0], -1)
-    # assert similarity_matrix.shape == labels.shape
-
-    # select and combine multiple positives
-    positives = similarity_matrix[labels.bool()].view(labels.shape[0], -1)
-
-    # select only the negatives the negatives
-    negatives = similarity_matrix[~labels.bool()].view(similarity_matrix.shape[0], -1)
-
-    logits = torch.cat([positives, negatives], dim=1)
-    labels = torch.zeros(logits.shape[0], dtype=torch.long).to(args.device)
-
-    logits = logits / args.temperature
-
-    loss = F.cross_entropy(logits, labels)
-
-    return loss
-
 def mac(x):
     return F.adaptive_max_pool2d(x, (1,1))
 
