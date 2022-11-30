b8127db5f2ec402e95aee987679dfdabaae5b958
xjh <xjh19972@gmail.com>
2022-11-30 15:40:07 -0500

    separate script

--- train_series.sh
+++ train_series.sh
@@ -3,13 +3,13 @@
 eval "$(conda shell.bash hook)"
 conda activate VTL
 
-# ./train_bing_esri_full.sh
+./train_bing_esri_full.sh
 
-# ./train_bing_google_full.sh
+./train_bing_google_full.sh
 
-# ./train_bing_foxtech_full.sh
+./train_bing_foxtech_full.sh
 
-# ./train_bing_thermal_full.sh
+./train_bing_thermal_full.sh
  
 ./train_bing_esri_partial.sh
 
@@ -18,3 +18,11 @@ conda activate VTL
 ./train_bing_foxtech_partial.sh
 
 ./train_bing_thermal_partial.sh 
+
+./train_bing_esri_partial_separate.sh
+
+./train_bing_google_partial_separate.sh 
+
+./train_bing_foxtech_partial_separate.sh
+
+./train_bing_thermal_partial_separate.sh 
