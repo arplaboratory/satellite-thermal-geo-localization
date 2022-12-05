bcb511e768102ffecdd617f1c63474b6ff9b42b7
xjh <xjh19972@gmail.com>
2022-12-05 15:12:31 -0500

    update script for neg=1

---
+++ train_bing_foxtech_partial_separate_neg1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+eval "$(conda shell.bash hook)"
+conda activate VTL
+
+python3 train.py --dataset_name=satellite_0_foxtechmapping_0 --mining=partial --datasets_folder=datasets --infer_batch_size 128 --train_batch_size 48 --lr 0.0001 --separate_branch --negs_num_per_query 1
--- train_series.sh
+++ train_series.sh
@@ -26,3 +26,11 @@ conda activate VTL
 ./train_bing_foxtech_partial_separate.sh
 
 ./train_bing_thermal_partial_separate.sh 
+
+./train_bing_esri_partial_separate_neg1.sh
+
+./train_bing_google_partial_separate_neg1.sh 
+
+./train_bing_foxtech_partial_separate_neg1.sh
+
+./train_bing_thermal_partial_separate_neg1.sh 
