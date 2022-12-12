e6004c532a8f69a618567853366135e9456dea3e
xjh <xjh19972@gmail.com>
2022-12-12 15:59:08 -0500

    update weight decay

--- train_bing_thermal_partial_separate.sh
+++ train_bing_thermal_partial_separate.sh
@@ -3,4 +3,4 @@
 eval "$(conda shell.bash hook)"
 conda activate VTL
 
-python3 train.py --dataset_name=satellite_0_thermalmapping_012345 --mining=partial --datasets_folder=datasets --infer_batch_size 128 --train_batch_size 12 --lr 0.0001 --separate_branch --patience 10
+python3 train.py --dataset_name=satellite_0_thermalmapping_012345 --mining=partial --datasets_folder=datasets --infer_batch_size 128 --train_batch_size 12 --lr 1e-4 --separate_branch --patience 7 --use_faiss_gpu --weight_decay 0.001
