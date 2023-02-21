f2ae67a5926d09bcd482b2023df7c70bc9f314bf
Jiuhong Xiao <jx1190@log-1.hpc.nyu.edu>
2023-02-21 10:43:29 -0500

    remove old train scripts

--- train_bing_foxtech_partial.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-eval "$(conda shell.bash hook)"
-conda activate VTL
-
-python3 train.py --dataset_name=satellite_0_foxtechmapping_0 --mining=partial --datasets_folder=datasets --infer_batch_size 128 --train_batch_size 12 --lr 0.0001 --patience 7 --use_faiss_gpu --weight_decay 0.001
