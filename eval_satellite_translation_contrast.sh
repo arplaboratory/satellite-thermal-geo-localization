2d25492b84b278afadd00a44d37d739270c36422
xjh <xjh19972@gmail.com>
2023-02-28 16:05:41 -0500

    rename

--- eval_satellite_translation_contrast.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-eval "$(conda shell.bash hook)"
-conda activate VTL
-
-python3 eval_pix2pix_generate_h5.py --resume='logs/default/'$1'/last_model.pth' --dataset_name=satellite_0_thermalmapping_135 --datasets_folder ./datasets --G_net unet_deep --G_gray --GAN_upsample bilinear --GAN_resize 1024 1024 --G_contrast
